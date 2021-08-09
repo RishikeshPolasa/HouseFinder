7a5e93fc1d10e21fe5e99e7fc2b5d4bb61854279
RishikeshPolasa <rishipolasa@gmail.com>
2021-08-09 15:38:48 +0530

    added header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import "./App.css";
 import Block from "./Block";
+import Header from "./Header";
 
 function App() {
   const blocks = [];
   for (let i = 0; i < 5; i++) {
     blocks.push(<Block />);
   }
-  return <div className="App">{blocks}</div>;
+  return (
+    <div className="App">
+      <Header />
+      {blocks}
+    </div>
+  );
 }
 
 export default App;
